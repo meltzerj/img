b4eda56a36001f8ec03aad1c91d3e25971ecb4a2
Justin Meltzer <meltzerj@wharton.upenn.edu>
2011-09-14 14:36:31 -0400

    fixed rotation

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -11,26 +11,23 @@ $(document).ready(function() {
     function myCompleteDesc() {
         var image = $(this);
         var link = $(this).parent();
-        var img_url = image.attr("src");
-        var url = link.attr("href");
-        var description = image.attr("data-desc");
-        link.replaceWith('<div class="desc-text center" data-img-url='+img_url+' data-url=' + url + ' style="background: white url(../images/bg.png) repeat top left;color:#8b8b8b;text-shadow: white 1px 1px 0px;height:190px;width:290px;padding:5px;">' + description + '</div>');
+        var desc_div = link.parent().find("div.center");
+        link.hide();
+        desc_div.show();
     }
     
     function myCompleteImg() {
-        var div = $(this);
-        var img_url = div.attr("data-img-url");
-        var url = div.attr("data-url");
-        var desc = div.text();
-        div.replaceWith('<a href='+url+'><img class="center" src='+img_url+' data-desc="' + desc + '" /></a>');
+        var desc_div = $(this);
+        var link = $(this).parent().find("img.center").parent();
+        desc_div.hide();
+        link.show();
     }
     
     $(".description_link").live("click", function(e){
         e.preventDefault();
         var image = $(this).parent().find("img.center");
-        var description = image.attr("data-desc");
         image.stop().rotate3Di(
-                '480',
+                '360',
                 300,
                 {
                     complete: myCompleteDesc
@@ -44,7 +41,7 @@ $(document).ready(function() {
         e.preventDefault();
         var div = $(this).parent().find("div.center");
         div.stop().rotate3Di(
-                '480',
+                '360',
                 300,
                 {
                     complete: myCompleteImg
